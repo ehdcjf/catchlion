8544f41c9a795a6136de9f72a4f1a96cd35b802d
ehdcjf <kdch0823@naver.com>
2024-01-17 15:12:38 +0900

    🏈✏️  물리엔진 havok  불러오기

---
+++ apps/habok/README.md
@@ -0,0 +1,34 @@
+```ts
+private async initHavok() {
+		/**
+		 *  node_modules에서 havok을 불러오는 과정에 문제가 있는 거 같음.
+		 *
+		 *  아마도   vite 가 최적화하는 과정에서 wasm 파일에 문제가 생기는 거 같음.
+		 *
+		 * vite.config에  optimizeDeps.exclude 설정을 했는데  잘 안됨.
+		 *
+		 * 그래서 그냥 public 모듈에 넣어놓고 불러오기로함.
+		 *
+		 * 모노레포 버리고 나중에 진짜로 만들 때,  설정하면 될 수도 있음.
+		 */
+		const wasmBinary = await fetch('./havok/HavokPhysics.wasm');
+		const wasmBinaryArrayBuffer = await wasmBinary.arrayBuffer();
+		const havokInterface = await HavokPhysics({ wasmBinary: wasmBinaryArrayBuffer });
+		// const havokInterface = await HavokPhysics();
+
+		const hk = new HavokPlugin(true, havokInterface);
+		this.scene.enablePhysics(new Vector3(0, -9.8, 0), hk);
+
+		const sphereAggregate = new PhysicsAggregate(
+			this.sphere,
+			PhysicsShapeType.SPHERE,
+			{
+				mass: 100,
+				restitution: 0.75,
+			},
+			this.scene
+		);
+
+		const groundAggragate = new PhysicsAggregate(this.gound, PhysicsShapeType.BOX, { mass: 0 }, this.scene);
+	}
+```
---
+++ apps/habok/vite.config.ts
@@ -0,0 +1,51 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+	root: __dirname,
+	cacheDir: '../../node_modules/.vite/habok',
+	optimizeDeps: {
+		exclude: ['../../node_modules/@babylonjs/havok'],
+	},
+	server: {
+		port: 4200,
+		host: 'localhost',
+	},
+
+	preview: {
+		port: 4300,
+		host: 'localhost',
+	},
+	publicDir: '../../public',
+	plugins: [nxViteTsPaths()],
+
+	// Uncomment this if you are using workers.
+	// worker: {
+	//  plugins: [ nxViteTsPaths() ],
+	// },
+
+	build: {
+		outDir: '../dist/habok',
+		reportCompressedSize: true,
+		//assetsDir: './public',
+		commonjsOptions: {
+			transformMixedEsModules: true,
+		},
+	},
+
+	test: {
+		globals: true,
+		cache: {
+			dir: '../../node_modules/.vitest',
+		},
+		environment: 'jsdom',
+		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+		reporters: ['default'],
+		coverage: {
+			reportsDirectory: '../coverage/habok',
+			provider: 'v8',
+		},
+	},
+});
